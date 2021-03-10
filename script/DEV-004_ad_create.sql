a4ed06c012c07d32b7b26db7f25ac39fe2c916ac
drive <driveall@mail.ru>
2021-03-10 20:24:35 +0200

    DEV-004_reznik: Added ad table;

---
+++ script/DEV-004_ad_create.sql
@@ -0,0 +1,19 @@
+CREATE TABLE public.ad
+(
+    id character varying COLLATE pg_catalog."default" NOT NULL,
+    name character varying COLLATE pg_catalog."default",
+    "desc" character varying COLLATE pg_catalog."default",
+    pic character varying COLLATE pg_catalog."default",
+    price integer,
+    userid character varying COLLATE pg_catalog."default",
+    city character varying COLLATE pg_catalog."default",
+    phone character varying COLLATE pg_catalog."default",
+    email character varying COLLATE pg_catalog."default",
+    rubric integer,
+    CONSTRAINT ad_pkey PRIMARY KEY (id)
+)
+
+    TABLESPACE pg_default;
+
+ALTER TABLE public.ad
+    OWNER to postgres;
