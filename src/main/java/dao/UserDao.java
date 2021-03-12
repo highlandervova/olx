c52a0c7b548392699e7f9ced394cd7573669be74
pani-marinka <pani_marinka@ukr.net>
2021-03-12 03:00:54 +0200

    DEV-010_DEV-011_DEV-012 Create user table with id, login, pass, city, phone, email, ads(varchar) columns; Create User entity.  Create UserDao with getByLogin, add, edit methods

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -9,7 +9,7 @@ public interface UserDao {
 
     User getByLogin(String login) ;
     boolean add(User u);
-    public boolean updateUser(User u ); //update fields pass,
+    public boolean updateUser(User u ); //update fields pass,city,phone,email
     public void getByLoginUser(String login); // check  - true, if not user
 }
 
