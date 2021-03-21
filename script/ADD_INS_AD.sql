1d77c2fcdae1445cf956a9dcda318dd9ad33de1c
pani-marinka <pani_marinka@ukr.net>
2021-03-21 15:02:01 +0200

    DEV-032 Add autofill for city, phone, email fields with user's home city ADD:PREPARE table AD for DEV-034

--- script/ADD_INS_AD.sql
+++ script/ADD_INS_AD.sql
@@ -1,18 +1,15 @@
-INSERT INTO public.ad (id,"name",descr,pic,price,userid,city,phone,email,rubric,addate, adtime) VALUES
+INSERT INTO public.ad (id,"name",descr,pic,price,userid,city,phone,email,rubric,date) VALUES
 ('1','lexusCT200h','good car ',
  'http://piccy.info/view3/14184985/92ebaaba08b769a2024dba08b8f01fdf/1200/',12000,
  (select id FROM public."user" order by id desc limit 1),'1','+36889','test',1,
-    TO_DATE('17/12/2020 12:15:15', 'DD/MM/YYYY'),
-    '12:15:16'),
+    TO_TIMESTAMP('17/12/2020 12:15:15', 'DD/MM/YYYY HH24:MI:SS')),
     ('2','CROSS_2016rx','cross car ',
     'http://piccy.info/view3/14184984/6d04fa963bd15903b3b2073494476dbf',10000,
     (select id FROM public."user" order by id desc limit 1),'1','+36889','test',1,
-    TO_DATE('17/12/2020 12:15:15', 'DD/MM/YYYY'),
- '12:15:17'),
+    TO_TIMESTAMP('17/12/2020 12:15:16', 'DD/MM/YYYY HH24:MI:SS')),
     ('3','Sedan_LexusIS300','sedan ',
     'http://piccy.info/view3/14184986/ff09521e4b453c6e1d7cd58b582df74c/1200/',10000,
     (select id FROM public."user" order by id desc limit 1),'2','+36889','test2',1,
-   TO_DATE('17/12/2020 12:15:15', 'DD/MM/YYYY'),
-   '12:15:18');
+   TO_TIMESTAMP('17/12/2020 12:15:18', 'DD/MM/YYYY HH24:MI:SS'));
 
 commit;
--- script/DEV-004_ad_create.sql
+++ script/DEV-004_ad_create.sql
@@ -1,5 +1,3 @@
-DROP TABLE AD CASCADE
-
 CREATE TABLE public.ad
 (
     id character varying COLLATE pg_catalog."default" NOT NULL,
@@ -12,13 +10,12 @@ CREATE TABLE public.ad
     phone character varying COLLATE pg_catalog."default",
     email character varying COLLATE pg_catalog."default",
     rubric integer,
-    addate date,
-    adtime time,
     CONSTRAINT ad_pkey PRIMARY KEY (id)
 )
 
     TABLESPACE pg_default;
 
-ALTER TABLE public.ad
+ ALTER TABLE public.ad
     OWNER to postgres;
 
+ALTER TABLE ad ADD COLUMN date TIMESTAMP
